5741c6c1ddb2d3a3788e0e9036fc1f6a8f7c5435
Abdalaziz Abadallah <abdalazizabdalluh3@gmail.com>
2023-01-10 10:38:10 +0200

    style: add useful and contants classes

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,4 @@
+class AppImages {
+  static const String _imagePath = 'assets/images/';
+  static const String _svgPath = 'assets/svg/';
+}
---
+++ lib/core/constants/app_themes.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+class ThemesApp {
+  static ThemeData light = ThemeData(
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: AppColors.white,
+    // appBarTheme: AppBarTheme(
+    //   backgroundColor: AppColors.white,
+    //   titleTextStyle: StylesApp.headline6.copyWith(
+    //     color: ColorsApp.black87,
+    //     fontWeight: FontWeight.w600,
+    //   ),
+    //   iconTheme: IconThemeData(
+    //     color: ColorsApp.primary,
+    //   ),
+    //   elevation: 0,
+    //   centerTitle: true,
+    // ),
+    colorScheme: const ColorScheme.light().copyWith(primary: AppColors.primary),
+  );
+
+  static ThemeData dark = ThemeData(
+    brightness: Brightness.dark,
+    // scaffoldBackgroundColor: ColorsApp.black87,
+    // primarySwatch: Colors.amber,
+    // appBarTheme: AppBarTheme(
+    //   color: ColorsApp.white,
+    //   elevation: 0,
+    //   centerTitle: true,
+    // ),
+  );
+}
