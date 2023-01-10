39d611783ae9656b20c0e362e556216c3b1c3bed
Abdalaziz Abadallah <abdalazizabdalluh3@gmail.com>
2023-01-10 11:42:48 +0200

    fix(SplashBinding): change lazyPut to put

--- lib/views/splash_view/splash_binding.dart
+++ lib/views/splash_view/splash_binding.dart
@@ -4,6 +4,6 @@ import 'package:get/get.dart';
 class SlpashBinding implements Bindings {
 @override
 void dependencies() {
-  Get.lazyPut<SplashController>(() => SplashController(),);
+  Get.put<SplashController>(SplashController(),);
   }
 }
