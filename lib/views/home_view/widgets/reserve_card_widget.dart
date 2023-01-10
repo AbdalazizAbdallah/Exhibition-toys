4274f64a6c464b826eaddedda69847d9901f7e6d
Abdalaziz Abadallah <abdalazizabdalluh3@gmail.com>
2023-01-10 11:55:55 +0200

    feat(home): creat custom reserve card widget

---
+++ lib/views/home_view/widgets/reserve_card_widget.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ReserveCardWidget extends StatelessWidget {
+  const ReserveCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        // image: DecorationImage(image: AssetImage())
+      ),
+    );
+  }
+}
